60a980f8f8af1e3b731d2e4d0cd0b60b6ec2ef3e
Ira <ira.ivashko.99@gmail.com>
2024-10-28 19:37:30 +0300

    Создание декоратора доступа

---
+++ main.py
@@ -0,0 +1,23 @@
+user_roles = None
+def acces_control(roles):
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if not any(role in user_roles for role in roles):
+                raise PermissionError(f"Отказано в доступе. Ни одна из ролей пользователя не имеет соответствующих прав.")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+@acces_control(roles=['admin', 'moderator'])
+def some_func():
+    print("Доступ открыт")
+
+
+try:
+    user_roles = 'admin'
+    some_func()
+except PermissionError as e:
+    print(e)
+
